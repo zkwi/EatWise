31312b35ca01a7e6693526081c727843253260db
zkwi <zkwi2010@gmail.com>
2026-05-10 20:06:04 +0800

    Prepare v1.0.0 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
